c50cd91093fd41d36f1096ba2427bba0d90e2280
Maxime Kwizera <maxkwiz8@gmail.com.com>
2022-04-09 08:25:47 -0700

    dcjncs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * printf will print what is in quotes
  * main - main block
- *return: 0
+ * return: 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,""\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
