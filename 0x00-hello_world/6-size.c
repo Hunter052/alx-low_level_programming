5071d80a24aabd8b15bc74f047089fb054f06859
Maxime Kwizera <maxkwiz8@gmail.com.com>
2022-04-09 08:52:14 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+printf(Size of a char:);
+printf(Size of an int:);
+printf(Size of a long int:);
+printf(Size of a long long int:);
+printf(Size of a float:);
+Return (0);
+}
