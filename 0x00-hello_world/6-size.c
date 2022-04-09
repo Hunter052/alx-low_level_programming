7543983afdb3c959a6555f6a387e799ccf4a8e73
Maxime Kwizera <maxkwiz8@gmail.com.com>
2022-04-09 08:55:50 -0700

    task updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 printf("Size of a char:");
-printf(Size of an int:);
-printf(Size of a long int:);
-printf(Size of a long long int:);
-printf(Size of a float:);
+printf("Size of an int:");
+printf("Size of a long int:");
+printf("Size of a long long int:");
+printf("Size of a float:");
 Return (0);
 }
