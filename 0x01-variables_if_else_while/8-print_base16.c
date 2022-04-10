3e6839043e4cc54ce6ab54eff758b5e54645fe7d
Maxime Kwizera <maxkwiz8@gmail.com.com>
2022-04-10 10:29:28 -0700

    update to task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * return always returns 0
+ */
+
+int main(void)
+{
+    int b;
+    
+    for (b = 0; b <= 15; b++)
+        printf ("%d", b);
+    printf ("\n");
+    return (0);
+}
