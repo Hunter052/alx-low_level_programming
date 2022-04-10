4383cb535cf8954e335db7f2b3e32fa04db2ec47
Maxime Kwizera <maxkwiz8@gmail.com.com>
2022-04-10 09:19:44 -0700

    update to task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+
+}
