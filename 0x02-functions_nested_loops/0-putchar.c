a2935b53fee72a588e4f0ba76d998cbc54c2dc11
Maxime Kwizera <maxkwiz8@gmail.com.com>
2022-04-12 11:42:09 -0700

    task update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print "_putchar" while using the function putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
