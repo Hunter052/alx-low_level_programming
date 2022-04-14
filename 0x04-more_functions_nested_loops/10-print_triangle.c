2e74225dea58730b44a8bb634ff4c9219b1194cb
Maxime Kwizera <maxkwiz8@gmail.com.com>
2022-04-14 13:12:27 -0700

    updated task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,7 +4,9 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
+
 void print_triangle(int size)
+
 {
 if (size <= 0)
 {
@@ -12,6 +14,7 @@ _putchar('\n');
 }
 else
 {
+
 int i, j;
 
 for (i = 1; i <= size; i++)
