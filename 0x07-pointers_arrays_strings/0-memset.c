b3681e0585c909fb3d6388f4186862c13be910d8
Maxime Kwizera <maxkwiz8@gmail.com.com>
2022-04-26 08:28:59 -0700

    additional changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
-#include "header.h"
+#include "holberton.h"
 
 /**
- * _memset - This fills memory with a constant byte
- * @s: memory area to fill
- * @b: constant byte to use when doing fill action
- * @n: bytes of memory area to fill
- * Return : the memory are filled
- */
- 
+  * _memset - Fill memory with a constant byte
+  * @s: memory area to fill
+  * @b: constant byte to fill
+  * @n: bytes of memory area to fill
+  *
+  * Return: the memory area filled
+  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int a;
-    
-    for (a = 0; a < n, a++)
-    {
-        s[a] = b;
-    }
-    
-    return (s);
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
 }
